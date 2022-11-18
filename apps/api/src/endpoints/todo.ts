742e014a0e70e84dfc0cfab2544432be11a13c0f
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-11-18 11:32:03 -0600

    feat(api): add an endpoint to list all of the todos

--- apps/api/src/endpoints/todo.ts
+++ apps/api/src/endpoints/todo.ts
@@ -1,7 +1,8 @@
-import { defaultEndpointsFactory } from 'express-zod-api';
+import { defaultEndpointsFactory, z } from 'express-zod-api';
+import { NotFoundError } from 'slonik';
 
 import { container, TASK_SERVICE_TOKEN } from '~app/container';
-import { createTodo, todo } from '~app/schemas/task';
+import { createTodo, Todo, todo } from '~app/schemas/task';
 
 export const createTodoEndpoint = defaultEndpointsFactory.build({
   method: 'post',
@@ -9,5 +10,25 @@ export const createTodoEndpoint = defaultEndpointsFactory.build({
   output: todo,
   async handler({ input }) {
     return container.get(TASK_SERVICE_TOKEN).createOne(input);
+  },
+});
+
+export const listAllTodoEndpoint = defaultEndpointsFactory.build({
+  method: 'get',
+  input: z.object({}),
+  output: z.object({ todos: z.array(todo) }),
+  async handler() {
+    let todos: Array<Todo> = [];
+    try {
+      todos = (await container
+        .get(TASK_SERVICE_TOKEN)
+        .listAll()) as unknown as Array<Todo>;
+    } catch (error) {
+      if (!(error instanceof NotFoundError)) {
+        throw error;
+      }
+    } finally {
+      return { todos };
+    }
   },
 });
