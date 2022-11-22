6cfc8770855421182d5177d757f0be7e463e17dd
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-11-22 13:34:22 -0600

    build: load .env files in the monorepo

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,9 @@ import { devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
- * https://github.com/motdotla/dotenv
+ * https://github.com/marcocesarato/dotenv-mono
  */
-// require('dotenv').config();
+require('dotenv-mono').load();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
